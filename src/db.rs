8f2f25927f1e8696f58677a59518a3240c6203c2
pmagaz <magazpablo@gmail.com>
2019-09-18 17:31:47 +0200

    Add fileHandler struct

--- src/db.rs
+++ src/db.rs
@@ -1,35 +1,47 @@
-use std::error::Error;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{BufRead, BufReader, Error, Write};
 use std::path::Path;
 
 #[derive(Debug)]
 pub struct Db {
+    pub handler: FileHandler,
+}
+
+#[derive(Debug)]
+pub struct FileHandler {
     file_name: String,
+    handler: File,
+}
+
+impl FileHandler {
+    pub fn new(file_name: String) -> FileHandler {
+        let handler;
+        let exists = Path::new(&file_name).exists();
+        if exists {
+            handler = File::open(&file_name).unwrap();
+        } else {
+            handler = File::create(&file_name).unwrap();
+        }
+        FileHandler {
+            file_name: file_name,
+            handler: handler,
+        }
+    }
 }
 
 impl Db {
     pub fn new(file_name: String) -> Db {
         Db {
-            file_name: file_name,
+            handler: FileHandler::new(file_name),
         }
     }
-
-    pub fn start(self) -> Result<Db, std::io::Error> {
+    /*pub fn start(self) -> Result<Db, std::io::Error> {
         if self.file_exits(&self.file_name) {
             Ok(self)
         } else {
-            let file = self.create_file(&self.file_name)?;
+            let handler = self.create_file(&self.file_name)?;
+            self.handler = handler;
             Ok(self)
         }
-    }
-
-    pub fn file_exits(&self, file_name: &String) -> bool {
-        Path::new(&file_name).exists()
-    }
-
-    pub fn create_file(&self, file_name: &String) -> std::io::Result<File> {
-        let file = File::create(&file_name)?;
-        Ok(file)
-    }
+    }*/
 }
