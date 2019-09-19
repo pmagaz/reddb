3b3a73ec8affbca0b80f049413d4fd14b2b76b52
pmagaz <magazpablo@gmail.com>
2019-09-19 19:58:33 +0200

    Add .rustfmt.toml

---
+++ src/dstore/error.rs
@@ -0,0 +1,19 @@
+quick_error! {
+    #[derive(Debug)]
+    pub enum DStoreError {
+        Io(err: ::std::io::Error) {
+            description(err.description())
+            display("IO error: {}", err)
+            cause(err)
+            from()
+        }
+        Poison {}
+        NotFound {}
+    }
+}
+
+impl<T> From<::std::sync::PoisonError<T>> for DStoreError {
+    fn from(_: ::std::sync::PoisonError<T>) -> DStoreError {
+        DStoreError::Poison
+    }
+}
