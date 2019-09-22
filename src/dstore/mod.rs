12c31ade813caa1f13bce20a6213910d306912a1
pmagaz <magazpablo@gmail.com>
2019-09-22 18:45:36 +0200

    Add basic diff stuff

--- src/dstore/mod.rs
+++ src/dstore/mod.rs
@@ -2,11 +2,12 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
 use std::fmt::Debug;
+use std::io::Read;
 use std::io::{BufRead, BufReader};
-use std::io::{Read, Seek, SeekFrom, Write};
+use std::io::{Seek, SeekFrom, Write};
 use std::path::Path;
 use std::result;
-use std::sync::{RwLock, RwLockWriteGuard};
+use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 mod error;
 mod handler;
@@ -14,12 +15,53 @@ mod handler;
 mod ser;
 use handler::Handler;
 pub type Result<T> = result::Result<T, error::DStoreError>;
+pub type DStoreHashMap = HashMap<String, Document>;
+pub type DStoreSaveHashMap = HashMap<String, Status>;
+
+#[derive(Debug)]
+pub enum Status {
+    Saved,
+    NotSaved,
+}
 
 #[derive(Debug)]
 pub struct DStore {
     //pub struct DStore<T: std::fmt::Debug + Eq + Hash + Serialize + DeserializeOwned> {
     handler: Handler,
+    toSave: ToSaveData,
     store: RwLock<HashMap<String, Document>>,
+}
+
+#[derive(Debug)]
+pub struct ToSaveData {
+    data: HashMap<String, Status>,
+    //updater: Updater
+}
+
+impl ToSaveData {
+    pub fn addToQue(&mut self, key: String, value: Status) {
+        self.data.insert(key, value);
+    }
+    pub fn changeValue(&mut self, key: &str, data: &mut DStoreSaveHashMap) {
+        let value = data.get_mut(key);
+    }
+    pub fn getNewMap<'a>(
+        &mut self,
+        map: RwLockReadGuard<'a, DStoreHashMap>,
+    ) -> RwLockReadGuard<'a, DStoreHashMap> {
+        let mut data = &self.data;
+        //let mapStore = self.store.read().unwrap();
+        //CONTINUE HERE
+        for (k, v) in &data {
+            let value = map.get(k).unwrap();
+            //self.changeValue(k, &mut data);
+            data.get_mut(k);
+            //let new_value = &data.get_mut(k);
+            println!("K{:?}", k);
+            println!("V{:?}", value);
+        }
+        map
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -43,10 +85,9 @@ impl DStore {
             .read_to_end(&mut buf)
             .unwrap();
 
-        let mut map: HashMap<String, Document> = HashMap::new();
+        let mut map: DStoreHashMap = HashMap::new();
 
-        let lines = &mut buf.lines();
-        for (_index, line) in lines.enumerate() {
+        for (_index, line) in buf.lines().enumerate() {
             let content = &line.unwrap();
             let doc: Document = serde_json::from_str(content)?;
             let key = doc.id.clone();
@@ -54,38 +95,77 @@ impl DStore {
         }
         Ok(Self {
             handler: handler,
+            toSave: ToSaveData {
+                data: HashMap::new(),
+            },
             store: RwLock::new(map),
         })
     }
 
     pub fn get(&self) -> Result<()> {
         let data = self.store.read()?;
-        println!("aaaaaaa{:?}", data.len());
+        //println!("aaaaaaa{:?}", data.len());
         Ok(())
     }
 
     //TODO RETURN RESULT
     //TODO IMPLEMENT ONLY PERSISTS CHANGES
-    pub fn put(&mut self, value: String) -> &mut DStore {
+    pub fn insert(&mut self, value: String) -> &mut DStore {
         {
-            let mut lock = self.store.write().unwrap();
+            let mut map = self.store.write().unwrap();
             let doc: Document = serde_json::from_str(&value).unwrap();
-            lock.insert("value".to_string(), doc);
+            let key = doc.id.clone();
+            map.insert(key.clone(), doc);
+            self.toSave.addToQue(key, Status::NotSaved);
         }
         self
     }
 
+    // pub fn getNewMap<'a>(
+    //     &mut self,
+    //     map: DStoreSaveHashMap,
+    // ) -> RwLockReadGuard<'a, DStoreHashMap> {
+    //     //let data = &mut self.data;
+    //     //let mapStore = self.store.read().unwrap();
+    //     let map2 = self.store.read().unwrap();
+
+    //     for (k, v) in map {
+    //         let value = map.get(&k);
+    //         //self.changeValue(k, &data);//data.get_mut(k);
+    //         //let new_value = &data.get_mut(k);
+    //         println!("K{:?}", k);
+    //         println!("V{:?}", value);
+    //     }
+    //     map2
+    // }
+
     pub fn persist(&mut self) -> Result<()> {
         let map = self.store.read()?;
         let mut file = self.handler.file.lock()?;
-        let buf = ser::serialize(&*map).unwrap();
-        println!("aaaaaaa{}", map.len());
+        let map2 = self.toSave.getNewMap(map);
+        let buf = ser::serialize(&*map2).unwrap();
+        //let data = &mut self.toSave;
+        // println!("aaaaaaa{:?}", map.len());
+        // for (k, v) in data {
+        //     println!("{:?}", k);
 
-        file.set_len(0);
-        file.seek(SeekFrom::Start(0));
-        file.write_all(&buf);
-        file.write_all(b"\n")?;
-        file.sync_all()?;
+        //     //  let doc = match map.get(&k) {
+        //     //     Some(val) => val,
+        //     //     None => "",
+        //     //  };
+        //     let value = map.get(k);
+        //     //let change = &*data.get_mut(k);
+
+        //     println!("{:?}", value);
+        // }
+
+        //let map : <HashMap<String, Document> = &self.save.map(
+
+        // file.set_len(0);
+        // file.seek(SeekFrom::Start(0));
+        // file.write_all(&buf);
+        // file.write_all(b"\n")?;
+        // file.sync_all()?;
         Ok(())
     }
 }
