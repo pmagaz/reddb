264248eeadc79ecf33c85c61d609bce57cb9f36d
pmagaz <magazpablo@gmail.com>
2020-02-01 10:17:58 +0100

    Update main

--- src/main.rs
+++ src/main.rs
@@ -7,48 +7,44 @@ use std::sync::{Arc, Mutex, MutexGuard, RwLock, RwLockReadGuard};
 use uuid::Uuid;
 type StoreHashMap<T> = HashMap<Uuid, T>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Document {
   pub data: Value,
+  //    data: RwLock<Hashmap>,
 }
 
 pub struct Store<T> {
-  pub store: RwLock<Arc<Mutex<StoreHashMap<T>>>>,
+  pub store: Arc<RwLock<Mutex<StoreHashMap<T>>>>,
 }
 
 impl<T> Store<T> {
   pub fn new() -> Self {
-    let mut hm = HashMap::new();
-    let map: Arc<Mutex<StoreHashMap<T>>> = Arc::new(Mutex::new(hm));
+    let hm = HashMap::new();
+    let map: RwLock<Mutex<StoreHashMap<T>>> = RwLock::new(Mutex::new(hm));
     Self {
-      //store: map,
-      store: RwLock::new(map),
+      store: Arc::new(map),
     }
   }
 
-  pub fn find_by_id<'a, 'b, 'c>(
+  pub fn find_by_id<'a, 'b: 'a, 'c>(
     &'b self,
-    map: &'a RwLockReadGuard<Arc<Mutex<StoreHashMap<T>>>>,
+    store: &'a RwLockReadGuard<Mutex<StoreHashMap<T>>>,
     id: &'c Uuid,
   ) -> MutexGuardRef<'a, StoreHashMap<T>, T> {
-    let guard = map.lock().unwrap();
-    MutexGuardRef::new(guard).map(|mg| {
+    let map = store.lock().unwrap();
+    MutexGuardRef::new(map).map(|mg| {
       mg.get(id)
         .ok_or_else(|| Error::new(ErrorKind::NotFound, "Not found"))
         .unwrap()
     })
   }
 
-  pub fn find_one<'a, 'b, 'c>(&'b self, id: &'c Uuid) -> MutexGuardRef<'a, StoreHashMap<T>, T> {
-    let read = self.store.read().unwrap();
-    let document = self.find_by_id(&read, &id);
-    document
+  pub fn find_one<'a, 'b: 'a, 'c>(&'b self, id: &'c Uuid) -> MutexGuardRef<'a, StoreHashMap<T>, T> {
+    //pub fn find_one<'a, 'b: 'a, 'c>(&'b self, id: &'c Uuid) -> Result<()> {
+    let store = self.store.read().unwrap();
+    let document = self.find_by_id(&store, &id);
+    document.clone()
   }
-
-  // pub fn insert<'a>(&self, key: Uuid, val: T) -> Option<Arc<Mutex<T>>> {
-  //   let mut write = self.store.write().unwrap();
-  //   write.insert(key, val)
-  // }
 }
 
 fn main() {
@@ -57,7 +53,6 @@ fn main() {
   let doc = Document {
     data: json!({"name":"Peter"}),
   };
-  //store.insert(id, doc);
   let document = store.find_one(&id);
   println!("{:?}", document);
 }
