760080012d55a02b17e4cddd0de4c93006ebb6c2
pmagaz <magazpablo@gmail.com>
2020-02-29 18:57:39 +0100

    Add record and handlers

---
+++ src/record.rs
@@ -0,0 +1,20 @@
+use uuid::Uuid;
+
+#[derive(Debug, Clone)]
+pub struct Record<T> {
+  pub _id: Uuid,
+  pub data: T,
+}
+
+pub trait Document<T>: Clone {
+  fn get_data(&self) -> &T;
+}
+
+impl<T> Document<T> for Record<T>
+where
+  T: Clone,
+{
+  fn get_data(&self) -> &T {
+    &self.data
+  }
+}
