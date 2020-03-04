54783221912f2a9395f64a7acb5fd1e9c6705432
pmagaz <magazpablo@gmail.com>
2020-03-04 12:50:12 +0100

    Add delete_one and delete_all methods

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,14 @@ mod storage;
 use status::Status;
 use std::fmt::Debug;
 use storage::Storage;
-use store_handler::{find_key, find_value, insert};
+use store_handler::Handler;
 
 pub type ReadJson<T> = RedDb<Document<T>>;
 
 #[derive(Debug)]
 pub struct RedDb<T> {
   pub store: Store<Document<T>>,
+  pub handler: Handler,
   pub storage: Storage,
 }
 
@@ -26,6 +27,7 @@ where
 {
   pub fn new() -> Self {
     Self {
+      handler: Handler {},
       store: Store::<Document<T>>::new(),
       storage: Storage::new(".db").unwrap(),
     }
@@ -35,20 +37,40 @@ where
     let mut store = self.store.to_write();
     let id = Uuid::new_v4();
     let doc = Document::new(id, value);
-    let result = insert::<T, Document<T>>(&mut store, doc);
+    let result = self.handler.insert::<T, Document<T>>(&mut store, doc);
     result
   }
 
-  pub fn find_one(&self, id: &Uuid) -> T {
+  pub fn find_one(&self, id: &Uuid) -> Document<T> {
     let store = self.store.to_read();
-    let doc = find_key::<Document<T>>(&store, &id);
-    self.storage.write(&doc.as_u8());
-    doc.get_data().clone()
+    let doc = self.handler.find_key::<Document<T>>(&store, &id);
+    doc.to_owned()
+    //doc.get_data().clone()
   }
 
-  pub fn find(&self, value: T) -> Vec<Document<T>> {
+  pub fn find_all(&self, value: T) -> Vec<Document<T>> {
     let store = self.store.to_read();
-    let docs = find_value::<T, Document<T>>(&store, value);
+    let docs = self
+      .handler
+      .find_from_value::<T, Document<T>>(&store, value);
+    docs
+  }
+
+  pub fn delete_one(&self, id: &Uuid) -> Document<T> {
+    let mut store = self.store.to_write();
+    let doc = self.handler.delete_key::<T, Document<T>>(&mut store, &id);
+    doc
+  }
+
+  pub fn delete_all(&self, value: T) -> Vec<Document<T>> {
+    let store = self.store.to_read();
+
+    let docs = self
+      .handler
+      .find_from_value::<T, Document<T>>(&store, value);
+
+    let deleted = docs.iter().map(|doc| self.delete_one(doc.get_id()));
+
     docs
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,32 @@ fn main() {
     leches: String,
   };
 
-  let db = RedDb::<MyStruct>::new();
-  let id = db.insert(MyStruct {
-    leches: String::from("hola"),
-  });
-  let result = db.find(MyStruct {
-    leches: String::from("hola"),
-  });
-  println!("FIND Struct {:?}", result);
+  // let db = RedDb::<MyStruct>::new();
+  // let id = db.insert(MyStruct {
+  //   leches: String::from("hola"),
+  // });
+
+  // let result = db.find_all(MyStruct {
+  //   leches: String::from("hola"),
+  // });
   // let result = db.find_one(&id);
   // println!("FIND_ONE {:?}", result);
 
   let db2 = RedDb::<Value>::new();
-  //let id = db.insert(json!({ "leches": true}));
-  //let result = db.find_one(&id);
-  //println!("FIND_ONE {:?}", result);
+  let id = db2.insert(json!({ "leches": true}));
+  let id = db2.insert(json!({ "leches": true, "boo": 12}));
+  let id2 = db2.insert(json!({ "leches": false}));
+  let result = db2.find_one(&id);
+  println!("FIND_ONE {:?}", result);
   db2.insert(json!({"name":"record1", "leches": 11}));
-  let result = db2.find(json!({"name":"record1", "leches": 11}));
-  println!("FIND JSON{:?}", result);
+  let result = db2.find_all(json!({"name":"record1", "leches": 11}));
+  println!("FIND ALL {:?}", result);
+  let id = db2.delete_one(&id2);
+  let result = db2.find_all(json!({ "leches": false}));
+  println!("FIND DELETED ONE {:?}", result);
+  let id = db2.delete_all(json!({ "leches": true}));
+  let result = db2.find_all(json!({ "leches": false}));
+  println!("FIND DELETED ALL {:?}", result);
+  let result = db2.find_all(json!({ "leches": true, "boo": 12}));
+  println!("FIND ALL {:?}", result);
 }
