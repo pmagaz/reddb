d0085848077af08741ff3895d8f1c976f8581c16
pmagaz <magazpablo@gmail.com>
2020-03-05 15:52:43 +0100

    Add json_serializer

---
+++ src/json_ser.rs
@@ -0,0 +1,23 @@
+use std::io::Read;
+
+pub use serde::de::{Deserialize, DeserializeOwned};
+use serde::Serialize;
+
+pub trait DeSerializer<'a, T: Serialize + Deserialize<'a>>:
+  ::std::default::Default + Send + Sync + Clone
+{
+  fn serialize(&self, val: &T) -> Vec<u8>;
+  fn deserialize<R: Read>(&self, s: R) -> T;
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct Json;
+
+impl<'a, T: Serialize + DeserializeOwned> DeSerializer<'a, T> for Json {
+  fn serialize(&self, value: &T) -> Vec<u8> {
+    serde_json::to_vec(value).unwrap()
+  }
+  fn deserialize<R: Read>(&self, value: R) -> T {
+    serde_json::from_reader(value).unwrap()
+  }
+}
