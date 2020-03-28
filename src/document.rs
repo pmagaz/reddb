4846c972ded400caf0906d050f3fc01baca14dc0
pmagaz <magazpablo@gmail.com>
2020-03-28 20:12:14 +0100

    Add storage

--- src/document.rs
+++ src/document.rs
@@ -14,8 +14,6 @@ pub trait Document<T>: Clone + Sized + Debug {
   fn get_status(&self) -> &Status;
   fn as_u8(&self) -> Vec<u8>;
   fn data_as_value(&self) -> Value;
-  fn find_content(&self, value: &T) -> bool;
-  fn update_content(&self, value: &T, new_value: &T) -> Value;
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -55,44 +53,6 @@ where
   fn set_data(&mut self, data: T) -> &Self {
     *&mut self.data = data;
     self
-  }
-  fn find_content(&self, query: &T) -> bool {
-    //FIXME pass serializer
-    let doc_object = serde_json::to_value(&self.get_data()).unwrap();
-    let query_object = serde_json::to_value(query).unwrap();
-    let query_fields = query_object.as_object().unwrap();
-    let mut matches: Vec<i32> = Vec::new();
-    for (prop, field) in query_fields.iter() {
-      match doc_object.get(prop) {
-        Some(val) => {
-          if val == field {
-            matches.push(1);
-          }
-        }
-        None => (),
-      };
-    }
-    query_fields.len() == matches.len()
-  }
-  fn update_content(&self, query: &T, new_value: &T) -> Value {
-    //FIXME pass serializer
-    let mut doc_object = serde_json::to_value(&self.get_data()).unwrap();
-    let query_object = serde_json::to_value(query).unwrap();
-    let new_object = serde_json::to_value(new_value).unwrap();
-    let query_fields = query_object.as_object().unwrap();
-    let mut matches: Vec<i32> = Vec::new();
-    for (prop, field) in query_fields.iter() {
-      match doc_object.get(prop) {
-        Some(val) => {
-          if val == field {
-            *doc_object.get_mut(prop).unwrap() = new_object.get(prop).unwrap().clone();
-            matches.push(1);
-          }
-        }
-        None => (),
-      };
-    }
-    doc_object
   }
   fn as_u8(&self) -> Vec<u8> {
     //FIXME pass serializer
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,90 @@
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
+use std::fmt;
 extern crate reddb;
 use reddb::{JsonSerializer, RedDb};
 
 fn main() {
-  #[derive(Debug, Clone, Serialize, Deserialize)]
+  #[derive(Clone, Debug, Serialize, PartialEq, Deserialize)]
   struct MyStruct {
-    leches: String,
+    foo: String,
+  }
+
+  impl fmt::Display for MyStruct {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+      write!(f, "({})", self)
+    }
+  }
+
+  //STRUCTS
+  println!("STRUCTS");
+  let query = MyStruct {
+    foo: String::from("hola"),
   };
-
+  let new_value = MyStruct {
+    foo: String::from("new Value"),
+  };
   let db = RedDb::<MyStruct, JsonSerializer>::new();
-  let _id = db.insert(MyStruct {
-    leches: String::from("hola"),
-  });
+  let _id = db.insert(query.clone());
   let id = db.insert(MyStruct {
-    leches: String::from("hola"),
+    foo: String::from("holaa"),
   });
   let result = db.find_one(&id);
   println!("FIND_ONE {:?}", result);
-  let result = db.find_all(MyStruct {
-    leches: String::from("hola"),
-  });
+  let result = db.find_all(&query);
   println!("FIND ALL {:?}", result);
+  let result = db.update_one(&_id, &new_value);
+  println!("FIND ONE UPDATED {:?}", result);
+  let result = db.update_all(&query, &new_value);
+  println!("UPDATE ALL {:?}", result);
 
+  println!("JSON STRINGS");
+  let db = RedDb::<String, JsonSerializer>::new();
+  let query = r#"
+        {
+            "leches": true,
+            "boo": 12,
+        }"#;
+
+  let query2 = r#"
+        {
+            "leches": false,
+            "boo": 12,
+        }"#;
+
+  let id = db.insert(query.to_owned());
+  println!("INSERT {:?}", &id);
   let result = db.find_one(&id);
   println!("FIND_ONE {:?}", result);
+  let result = db.find_all(&query2.to_owned());
+  println!("FIND ALL {:?}", result);
+  //JSON VALUES
+  //println!("JSON VALUES");
 
-  let db2 = RedDb::<Value, JsonSerializer>::new();
-  let _id = db2.insert(json!({ "leches": true}));
-  let id = db2.insert(json!({ "leches": true, "boo": 12}));
-  let id2 = db2.insert(json!({ "leches": false}));
-  let result = db2.find_one(&id);
-  println!("FIND_ONE {:?}", result);
-  db2.insert(json!({"name":"record1", "leches": 11}));
-  let result = db2.find_all(json!({"name":"record1", "leches": 11}));
-  println!("FIND ALL {:?}", result);
-  db2.delete_one(&id2);
-  let result = db2.find_all(json!({ "leches": false}));
-  println!("FIND DELETED ONE {:?}", result);
-  db2.delete_all(json!({ "leches": true}));
-  let result = db2.find_all(json!({ "leches": false}));
-  println!("FIND DELETED ALL {:?}", result);
-  let result = db2.find_all(json!({ "leches": true, "boo": 12}));
-  println!("FIND ALL {:?}", result);
-  let id3 = db2.insert(json!({ "record": true, "foo": 11}));
-  db2.update_one(&id3, json!({ "record": "updateeeed"}));
-  let result = db2.find_one(&id3);
-  println!("UPDATED ONE {:?}", result);
-  db2.update_all(
-    json!({ "record": "updateeeed"}),
-    json!({ "record": "updated!"}),
-  );
-  let result = db2.find_all(json!({ "record": "updated!"}));
-  println!("UPDATED ALL {:?}", result);
+  // let db2 = RedDb::<Value, JsonSerializer>::new();
+  // let _id = db2.insert(json!({ "leches": true}));
+  // let id = db2.insert(json!({ "leches": true, "boo": 12}));
+  // let id2 = db2.insert(json!({ "leches": false}));
+  // let result = db2.find_one(&id);
+  // println!("FIND_ONE {:?}", result);
+  // db2.insert(json!({"name":"record1", "leches": 11}));
+  // let result = db2.find_all(json!({"name":"record1", "leches": 11}));
+  // println!("FIND ALL {:?}", result);
+  // db2.delete_one(&id2);
+  // let result = db2.find_all(json!({ "leches": false}));
+  // println!("FIND DELETED ONE {:?}", result);
+  // db2.delete_all(json!({ "leches": true}));
+  // let result = db2.find_all(json!({ "leches": false}));
+  // println!("FIND DELETED ALL {:?}", result);
+  // let result = db2.find_all(json!({ "leches": true, "boo": 12}));
+  // println!("FIND ALL {:?}", result);
+  // let id3 = db2.insert(json!({ "record": true, "foo": 11}));
+  // db2.update_one(&id3, json!({ "record": "updateeeed"}));
+  // let result = db2.find_one(&id3);
+  // println!("UPDATED ONE {:?}", result);
+  // db2.update_all(
+  //   json!({ "record": "updateeeed"}),
+  //   json!({ "record": "updated!"}),
+  // );
+  // let result = db2.find_all(json!({ "record": "updated!"}));
+  // println!("UPDATED ALL {:?}", result);
 }
