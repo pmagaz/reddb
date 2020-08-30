2de9d8ab7ce75d278bf496a48445e104b77b6810
pmagaz <magazpablo@gmail.com>
2020-08-30 20:08:22 +0200

    Add tests

--- src/document.rs
+++ src/document.rs
@@ -5,7 +5,7 @@ use uuid::Uuid;
 
 #[derive(Debug, Serialize, Clone, PartialEq, Deserialize)]
 pub struct Document<T> {
-  pub _id: Uuid,
+  pub id: Uuid,
   pub data: T,
 }
 
@@ -13,7 +13,7 @@ impl<'a, T> Document<T>
 where
   T: Serialize + Deserialize<'a> + Debug,
 {
-  pub fn new(_id: Uuid, data: T) -> Self {
-    Self { _id, data }
+  pub fn new(id: Uuid, data: T) -> Self {
+    Self { id, data }
   }
 }
