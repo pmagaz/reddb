951cf3d5e86622df61b8759f808f4ded53ed00cb
pmagaz <magazpablo@gmail.com>
2020-12-25 11:04:26 +0100

    Remove mutex and update tests

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,5 +1,6 @@
 use crate::error::Result;
 use crate::RedDbHM;
+use async_trait::async_trait;
 use core::fmt::Debug;
 use serde::{Deserialize, Serialize};
 use std::marker::Sized;
@@ -9,6 +10,7 @@ use crate::document::Document;
 
 pub use file::FileStorage;
 
+#[async_trait]
 pub trait Storage {
   fn new(db_name: &str) -> Result<Self>
   where
@@ -16,7 +18,7 @@ pub trait Storage {
   fn load<T>(&self) -> Result<RedDbHM>
   where
     for<'de> T: Serialize + Deserialize<'de> + Debug + PartialEq;
-  fn persist<T>(&self, records: &[Document<T>]) -> Result<()>
+  async fn persist<T>(&self, records: &[Document<T>]) -> Result<()>
   where
-    for<'de> T: Serialize + Deserialize<'de> + Debug;
+    for<'de> T: Serialize + Deserialize<'de> + Debug + Sync;
 }
