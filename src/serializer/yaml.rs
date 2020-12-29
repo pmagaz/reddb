2dd7dee28f02a7fd952e78400220f3a834f20e14
pmagaz <magazpablo@gmail.com>
2020-12-29 17:32:17 +0100

    Add feature flags

--- src/serializer/yaml.rs
+++ src/serializer/yaml.rs
@@ -1,9 +1,7 @@
-use failure::Error;
-use serde::{Deserialize, Serialize};
 use std::default::Default;
 use std::fmt::Debug;
 
-use super::{Serializer, Serializers};
+use super::*;
 
 #[derive(Debug)]
 pub struct Yaml {
@@ -18,9 +16,7 @@ impl Default for Yaml {
     }
 }
 
-pub type YamlSerializer = Yaml;
-
-//ca#[cfg(feature = "yaml_ser")]
+#[cfg(feature = "yaml_ser")]
 impl<'a> Serializer<'a> for Yaml {
     fn format(&self) -> &Serializers {
         &self.format
@@ -28,7 +24,7 @@ impl<'a> Serializer<'a> for Yaml {
 
     fn serialize<T>(&self, data: &T) -> Result<Vec<u8>, Error>
     where
-        for<'de> T: Serialize + Deserialize<'de>,
+        for<'de> T: serde::Serialize + serde::Deserialize<'de>,
     {
         let mut vec = serde_yaml::to_vec(data).unwrap();
         vec.extend(b"\n");
@@ -36,7 +32,7 @@ impl<'a> Serializer<'a> for Yaml {
     }
     fn deserialize<T>(&self, data: &[u8]) -> Result<T, Error>
     where
-        for<'de> T: Serialize + Deserialize<'de>,
+        for<'de> T: serde::Serialize + serde::Deserialize<'de>,
     {
         Ok(serde_yaml::from_slice::<T>(data).unwrap())
     }
